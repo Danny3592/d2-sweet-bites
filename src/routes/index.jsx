b29668f5230d8f5daa69369e4ebd00b7728a324d
shosetim <shosetim32@gmail.com>
2025-02-09 22:54:34 +0800

    刪除 routes loader

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,10 @@
-/* eslint-disable no-unused-vars */
+ 
 import FrontLayout from "../pages/front/FrontLayout";
 import Home from "../pages/front/Home";
 import ProductList from "../pages/front/ProductList";
 import AdminLogin from "../pages/dashboard/AdminLogin";
 import AdminRegister from "../pages/dashboard/AdminRegister";
-import AdminProducts, {
-  loader as productsLoader,
-} from "../pages/dashboard/AdminProducts";
-// import Dashboard from '../pages/dashboard/Dashboard';  是否還需要留著
+import AdminProducts from "../pages/dashboard/AdminProducts";
 import NotFound from "../pages/NotFound";
 import { createHashRouter } from "react-router-dom";
 import App from "../App";
@@ -46,7 +43,6 @@ const routes = [
           {
             index: true,
             element: <AdminProducts />,
-            // loader:productsLoader
           },
         ],
       },
