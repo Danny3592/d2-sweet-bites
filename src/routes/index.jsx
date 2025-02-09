2f45aa112be8ee943dc2081583b29ec3ba1e1874
joylululu <joylululu@gmail.com>
2025-02-09 11:11:18 -0800

    加入登入頁面, 修改登入註冊入徑

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,10 +3,7 @@ import Home from "../pages/front/Home";
 import ProductList from "../pages/front/ProductList";
 import AdminLogin from "../pages/dashboard/AdminLogin";
 import AdminRegister from "../pages/dashboard/AdminRegister";
-import AdminProducts, {
-  loader as productsLoader,
-} from "../pages/dashboard/AdminProducts";
-// import Dashboard from '../pages/dashboard/Dashboard';  是否還需要留著
+
 import NotFound from "../pages/NotFound";
 import { createHashRouter } from "react-router-dom";
 import App from "../App";
@@ -41,13 +38,7 @@ const routes = [
       {
         path: "dashboard",
         element: <AdminLayout />,
-        children: [
-          {
-            index: true,
-            element: <AdminProducts />,
-            // loader:productsLoader
-          },
-        ],
+        children: [],
       },
       {
         path: "*",
