4593553bb61a3e45330dc297be069c4be9012919
shosetim <shosetim32@gmail.com>
2025-02-19 14:09:23 +0800

    完成前台會員中心我的收藏頁面

--- src/pages/front/UserFavorite.jsx
+++ src/pages/front/UserFavorite.jsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from "react"
 import { NavLink } from "react-router-dom"
+import { alertError, alertDeleteConfirm } from "../../../util/sweetAlert";
+import Loading from "../../components/Loading";
+import Pagination from '../../components/Pagination';
+import axios from 'axios';
 export default function UserFavorite() {
+  const [favoriteProducts, setFavoriteProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const getFavoriteProducts = async (page = 1) => {
+    setIsLoading(true);
+    try {
+      const res = await axios.get(`/600/users/1/favorites?_expand=product&_page=${page}&_limit=10`);
+      setTotalPages(Math.ceil(res.headers.get("X-Total-Count") / 10));
+      setFavoriteProducts(res.data);
+    } catch(error) {
+      alertError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  useEffect(() => {
+    getFavoriteProducts(currentPage);
+  }, [currentPage]);
+
+  // 刪除收藏商品
+  const deleteFavoriteProduct = async (favoriteId, productName) => {
+    const res = await alertDeleteConfirm(`確認刪除 ${productName} 嗎`);
+    if (res.isConfirmed) {
+      setIsLoading(true);
+      try {
+        await axios.delete(`/600/favorites/${favoriteId}`);
+        await getFavoriteProducts();
+      } catch(error) {
+        alertError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  }
+
   return (
     <>
+      { isLoading && (<Loading type="spin" color="#D4A58E"/>)  }
       <h1 className="fs-4 mb-8 noto-sans-tc">我的收藏</h1>
       <div className="table-responsive">
         <table className="table bg-transparent">
@@ -14,22 +56,31 @@ export default function UserFavorite() {
             </tr>
           </thead>
           <tbody className="bg-transparent">
-            <tr>
-              <th scope="row" className="p-3 ps-10">蛋糕</th>
-              <td className="p-3">莓舞輕雲</td>
-              <td className="p-3">420</td>
-              <td className="p-3">
-                <NavLink to="/" className="btn btn-sm btn-outline-primary me-4">
-                  立即訂購
-                </NavLink>
-                <button type="button" className="btn btn-sm btn-primary">
-                  移除收藏
-                </button>
-              </td>
-            </tr>
+            {favoriteProducts.map((item) => (
+              <tr key={item.product.id}>
+                <th scope="row" className="p-3 ps-10">{item.product.category}</th>
+                <td className="p-3">{item.product.title}</td>
+                <td className="p-3">{item.product.price}</td>
+                <td className="p-3">
+                  <NavLink to={`/product-details/${item.product.id}`}
+                    className="btn btn-sm btn-outline-primary me-4">
+                    立即訂購
+                  </NavLink>
+                  <button type="button" className="btn btn-sm btn-primary"
+                    onClick={() => deleteFavoriteProduct(item.id, item.product.title)}>
+                    移除收藏
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        setCurrentPage={setCurrentPage}
+      />
     </>
   )
 }
--- src/pages/front/UserLayout.jsx
+++ src/pages/front/UserLayout.jsx
@@ -1,7 +1,26 @@
 import { Outlet } from "react-router-dom";
 import mainLogo from '../../assets/images/user/main-logo.svg'
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 export default function UserLayout() {
+  const token = document.cookie
+  .split('; ')
+  .find(row => row.startsWith('dessertToken='))
+  ?.split('=')[1];
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+  const login = async () => {
+    try {
+      const res = await axios.post('/login', {
+        email: 'Shin@gmail.com',
+        password: '123456',
+      });
+      const { accessToken } = res.data;
+      document.cookie = `dessertToken=${accessToken}; max-age=86400;`;
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <>
       <div className="position-fixed top-0 start-0 vh-100 bg-primary p-10">
@@ -39,7 +58,8 @@ export default function UserLayout() {
         marginLeft: '240px',
       }}>
         <div className="d-flex justify-content-end mb-10">
-          <button className="btn btn-primary py-3">登出</button>
+          <button className="btn btn-primary py-3"
+            onClick={login}>登出</button>
         </div>
         <div>
           <Outlet />
