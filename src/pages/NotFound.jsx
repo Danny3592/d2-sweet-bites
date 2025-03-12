08aa7b91743a666aa357e6ee99e8001b9e49da2e
shosetim <shosetim32@gmail.com>
2025-03-12 14:18:30 +0800

    加入404頁面

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,19 @@
+import TheHeader from "../components/TheHeader";
+import TheFooter from "../components/TheFooter";
+import { Link } from "react-router-dom";
 export default function NotFound() {
   return (
-    <div>
-      404 頁面
-    </div>
+    <>
+      <TheHeader />
+      <div className="vh-body bg-primary-50 d-flex flex-column justify-content-center align-items-center">
+        <h1 className="mb-3 fs-1">404</h1>
+        <p className="fs-3 mb-6">糟糕! 您尋找的頁面不存在</p>
+        <Link to="/"
+          className="btn btn-primary">
+          返回首頁
+        </Link>
+      </div>
+      <TheFooter />
+    </>
   )
 }
